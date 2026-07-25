bf9e85d58e9fea27719665663792f291c1cb9e7a
Premysl Vaculik <premysl.vaculik@gmail.com>
2026-07-25 13:07:19 +0200

    Update default Task Scheduler execution time to 17:00

--- Nastavit_Denni_Planovac.bat
+++ Nastavit_Denni_Planovac.bat
@@ -6,15 +6,15 @@ echo     Update Seeker - Automaticka denni kontrola
 echo =======================================================
 echo.
 echo Tento skript nastavi v Planovaci uloh ve Windows automaticke 
-echo spusteni kontroly kazdy den v 09:00 na pozadi.
+echo spusteni kontroly kazdy den v 17:00 na pozadi.
 echo.
 
-schtasks /create /tn "UpdateSeekerDailyCheck" /tr "wscript.exe \"%~dp0Spustit_Na_Pozadi.vbs\"" /sc daily /st 09:00 /f
+schtasks /create /tn "UpdateSeekerDailyCheck" /tr "wscript.exe \"%~dp0Spustit_Na_Pozadi.vbs\"" /sc daily /st 17:00 /f
 
 if %errorlevel% equ 0 (
     echo.
     echo [OK] Uloha byla uspesne pridana do Planovace uloh ve Windows!
-    echo Kontrola probehne automaticky kazdy den v 09:00 na pozadi.
+    echo Kontrola probehne automaticky kazdy den v 17:00 na pozadi.
 ) else (
     echo.
     echo [CHYBA] Pridani ulohy selhalo. Spustte tento soubor jako Spravce (Administrator).
